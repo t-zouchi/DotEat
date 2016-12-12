c434f4fd08b48f404b1e76f1e41571690f5d20e0
t-zouchi <mojamoja.000@gmail.com>
2016-12-12 19:21:47 +0900

    update pointmanager

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -12,7 +12,6 @@ public class PointManager : MonoBehaviour {
   int point;
   int level;
 
-  // Use this for initialization
   void Start () {
     life = 3;
     point = 0;
@@ -69,6 +68,5 @@ public class PointManager : MonoBehaviour {
         pointText.text = "Point : " + point + " / " + level * 50;
       }
     }
-
   }
 }
