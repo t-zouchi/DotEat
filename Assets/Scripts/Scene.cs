05dbc4b2287eaa49abbae5f66fd322b5170af488
t-zouchi <mojamoja.000@gmail.com>
2016-12-12 19:25:17 +0900

    update scene

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class Scene : MonoBehaviour {
-
-  // Use this for initialization
+  
   public void start()
   {
     SceneManager.LoadScene("Main");
